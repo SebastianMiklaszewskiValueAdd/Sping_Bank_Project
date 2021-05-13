b02d7b123f3bbafeac9767a9ddfdd6c7cb24e56c
Sebastian Miklaszewski <sebastian.miklaszewski@valueadd.pl>
2021-05-13 22:00:42 +0200

    init commit

---
+++ src/main/java/com/example/spring_bank_project/SpringBankProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_bank_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBankProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBankProjectApplication.class, args);
+    }
+
+}
